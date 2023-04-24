9c6360e0420526a39fc096b54c72db4221025ef7
MailonlKanonl <marodrig@ksr.ch>
2023-04-24 22:27:07 +0200

    version 1 fertig, begonnen an version 2 zu arbeiten

---
+++ asteorids_version2.py
@@ -0,0 +1,50 @@
+#Noch nicht ganz fertig
+from microbit import *
+import time
+import random
+
+x = 2
+xr = random.randint(0,4)
+y = 0
+i = 0
+
+while True:
+    #Mein Schiff
+    display.set_pixel(x, 4, 9)
+
+    #Bewegung von Mein Schiff nach linggs
+    if button_a.get_presses() > 0:
+        display.set_pixel(x, 4, 0)
+        x -= 1
+        if x < 0:
+            x = 0
+
+    #Bewegung von Mein Schiff nach rechts
+    if button_b.get_presses() > 0:
+        display.set_pixel(x, 4, 0)
+        x += 1
+        if x > 4:
+            x = 4
+    time.sleep(0.1)
+
+    #Flüügende Brocke
+    display.set_pixel(xr, y, 3)
+    i += 1
+    if i == 15:
+        y += 1
+        i = 0
+        display.set_pixel(xr, y-1, 0)
+        if y >= 5:
+            y = 0
+            xr = random.randint(0,4)
+
+    #If Mein Schiff trifft auf Flüügende Brocke
+    if x == xr and y == 4:
+        display.show(Image.SKULL)
+        time.sleep(5)
+        display.clear()
+        x = 2
+        xr = random.randint(0,4)
+        y = 0
+        i = 0
+
